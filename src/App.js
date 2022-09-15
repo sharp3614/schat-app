c30c9a3280704b6f7fd24967695864af210f5c7f
sharp3614 <knurhak09@gmail.com>
2022-09-15 11:42:59 +0300

    Added uuid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AuthLayout from './layouts/authLayout';
+import Layout from './layouts/layout';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
@@ -8,9 +10,15 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
       </Routes>
     </div>
   );
