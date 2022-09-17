0004b72f735efb31b4485c7f82d3f90aa1d65561
sharp3614 <knurhak09@gmail.com>
2022-09-17 10:38:09 +0300

    Added settings page

--- src/pages/setting/index.js
+++ src/pages/setting/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/useAuth'
+
+import Style from './style.module.scss'
 
 const Setting = () => {
+  const { currentUser } = useAuth()
+  const navigate = useNavigate()
   return (
-    <div>Hi, Setting!</div>
+    <div className={Style.Setting}>
+      <div className={Style.back} >
+        <button className={Style.btn_back} onClick={
+          () => navigate("/")
+        } >X</button>
+      </div>
+      <div>
+        <img className={Style.avatar_update} src={currentUser.photoURL} alt="avatar" />
+      </div>
+    </div>
+
   )
 }
 
